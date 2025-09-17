eda94e083004dc9982da460f3dfa92d79c2bd7f4
24ftt1849 <24FTT1849@student.pb.edu.bn>
2025-09-17 10:59:06 +0000

    Add files via upload

--- P7/OccurenceOfMaxNumbers.java
+++ P7/OccurenceOfMaxNumbers.java
@@ -7,41 +7,33 @@ public class OccurenceOfMaxNumbers {
         Scanner scanner = new Scanner(System.in);
         int largest = 0;
         int count = 0;
-        int prev = 0;
-        
-        System.out.println("Enter numbers, the input ends with 0 entered:");
-        int num = scanner.nextInt();
+        int num = 0;
+
+        System.out.print("Enter numbers, the input ends with 0 entered: ");
+        num = scanner.nextInt();
 
         if(num == 0){
             System.out.println("The occurrence count of the largest number is 0");
         }
         else{
-        while (num != 0) {
-            prev = num;
-            System.out.println("Enter numbers, the input ends with 0 entered:");
+            largest = num;
+            count++;
+        do {
+            System.out.print("Enter numbers, the input ends with 0 entered: ");
             num = scanner.nextInt();
 
-            if(num>prev){
-                if(largest == 0){
+                if(num>largest){
                 largest = num;
-                count++;
+                count = 1;
             }
                 else if(num == largest){
                 count++;
-            }
-                else if(num>largest){
-                largest = num;
-                count = 0;
-            }
-                else if(largest == 0){
-                largest = num;
-                count++;
-            }
-        }
-    }
+        } 
+    } while(num != 0);
     System.out.println("The largest number is " + largest);
     System.out.println("The occurence count of the largest number is " + count);
-    }
     scanner.close();
+    }
 }
 }
+
